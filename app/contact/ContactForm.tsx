9655287659e760c36d15e0e7248c9b747128543a
Cleosetric <87144416+Cleosetric@users.noreply.github.com>
2026-05-09 17:57:21 +0700

    fix import missing package

--- app/contact/ContactForm.tsx
+++ app/contact/ContactForm.tsx
@@ -2,7 +2,6 @@
 
 import { useMemo, useState } from "react";
 import Recaptcha from "./Recaptcha";
-import { SimpleEditor } from '@/components/tiptap-templates/simple/simple-editor'
 
 type Status = "idle" | "sending" | "sent" | "error";
 
